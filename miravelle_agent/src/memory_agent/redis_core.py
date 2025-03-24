0a000405655cf6e68f4cf1553ec9717ee5085356
eungyu kim <86348187+eungyukm@users.noreply.github.com>
2025-03-24 19:10:12 +0900

    feat: add command loading and Redis integration
    - Add commands.json to Dockerfile and docker-compose.yml
    - Implement command loading and listing in FastAPI using Redis
    - Mount commands.json in docker-compose for dynamic updates

--- miravelle_agent/src/memory_agent/redis_core.py
+++ miravelle_agent/src/memory_agent/redis_core.py
@@ -1,13 +1,37 @@
-from fastapi import HTTPException, APIRouter
+from fastapi import APIRouter
 import redis
+import json
+import os
 
 router = APIRouter()
 
 redis_client = redis.Redis(host='redis_service', port=6379, decode_responses=True)
 
-@router.get("/")
-async def read_root():
-    return {"message": "Hello, FastAPI with Redis!"}
+
+def load_commands_to_redis():
+    # 절대 경로로 commands.json 로드
+    file_path = os.path.join("/app", "commands.json")
+    with open(file_path, "r") as file:
+        commands = json.load(file)
+        for key, value in commands.items():
+            redis_client.set(key, value)
+            print(f"{key} 명령어 저장 완료!")
+
+@router.post(("/startup"))
+def startup():
+    load_commands_to_redis()
+
+@router.get("/list-commands/")
+def list_commands():
+    keys = redis_client.keys('command_*')
+
+    # key가 bytes 타입인지 확인하고 decode 처리
+    commands = {
+        key.decode('utf-8') if isinstance(key, bytes) else key:
+            redis_client.get(key).decode('utf-8') if isinstance(redis_client.get(key), bytes) else redis_client.get(key)
+        for key in keys
+    }
+    return {"commands": commands}
 
 @router.post("/set/{key}/{value}")
 async def set_key(key: str, value: str):
